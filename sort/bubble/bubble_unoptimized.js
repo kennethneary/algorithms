f1c92a8a5844cf38b1184b6fae8120a64731b7bf
Kenneth <kennethneary@gmail.com>
2022-08-27 22:07:56 +0100

    add bubble sort

---
+++ sort/bubble/bubble_unoptimized.js
@@ -0,0 +1,30 @@
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
+
+function bubbleSortV2(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSortV2([8, 1, 2, 3, 4, 5, 6, 7]));
